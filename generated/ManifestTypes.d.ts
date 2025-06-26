a2d260f3d0b00d097f2444979140385877b3a24c
Prathamesh Pradhan <pradhan.prathamesh10@gmail.com>
2025-06-26 15:11:07 +0530

    Add files via upload

---
+++ generated/ManifestTypes.d.ts
@@ -0,0 +1,13 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    hostingField: ComponentFramework.PropertyTypes.StringProperty;
+    JsonMapping: ComponentFramework.PropertyTypes.StringProperty;
+    FocusedRecordId: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    hostingField?: string;
+}
